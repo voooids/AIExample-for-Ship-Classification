b6fd7d667ebe44ecce70f8eb499d4b8850d2b3bf
voooids <84967995+voooids@users.noreply.github.com>
2021-12-12 12:23:12 +0000

    Create Libraries.py

---
+++ PythonCodeFile/Libraries.py
@@ -0,0 +1,9 @@
+# 1. import All libraries 
+import tensorflow as tf
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.utils import plot_model
+from tensorflow.keras.models import load_model
+import numpy as np
+from sklearn.model_selection import train_test_split
+import os
+import matplotlib.pyplot as plt
